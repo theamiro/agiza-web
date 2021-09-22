d127db8a9471b982e08c592f24e68872b78462e0
Michael Amiro <odindo.michael@gmail.com>
2021-09-22 21:22:40 +0300

    Increase test coverage of Mutations' Completed the list of mutation tests'

--- tests/unit/mutations.spec.js
+++ tests/unit/mutations.spec.js
@@ -1,5 +1,6 @@
 import { mutations } from "@/store/mutations"
-import { mockCustomers } from "@/mock"
+import { mockUsers } from "@/mock"
+import { Role } from "@/helpers/Role"
 
 describe("mutations", () => {
 	it("should set a token", () => {
@@ -21,9 +22,9 @@ describe("mutations", () => {
 	})
 	it("should set customers", () => {
 		const state = { customers: [] }
-		const customers = mockCustomers
+		const customers = mockUsers.filter((user) => Role.Customer === user.role)
 		mutations.setCustomers(state, customers)
-		expect(state.customers.length).toBe(6)
+		expect(state.customers.length).toBe(2)
 	})
 	it("should Set Customer Form", () => {
 		const state = { customerForm: {} }
@@ -44,5 +45,30 @@ describe("mutations", () => {
 				country: "Kenya",
 			},
 		})
+	})
+	it("should clear customer Form", () => {
+		const state = {
+			customerForm: {
+				firstName: "John",
+				lastName: "Doe",
+				emailAddress: "john@mail.com",
+				phoneNUmber: "+25470999999",
+				country: "Kenya",
+			},
+		}
+		mutations.clearCustomerForm(state)
+		expect(state.customerForm).toEqual({})
+	})
+	it("should set User", () => {
+		const state = { currentUser: {} }
+		const user = { emailAddress: "agent@agent.com", password: "agent" }
+		mutations.setCurrentUser(state, user)
+		expect(state.currentUser).toEqual({ emailAddress: "agent@agent.com", password: "agent" })
+	})
+	it("should place order", () => {
+		const state = { orders: [] }
+		const product = { id: "1", title: "Baseball bat" }
+		mutations.placeOrder(state, product)
+		expect(state.orders.length).toBe(1)
 	})
 })
